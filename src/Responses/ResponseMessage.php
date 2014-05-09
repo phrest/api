4ea2475aa351e9f26ba92ebc8c4d7d405cda2285
Jay Francis <jay@whistlingmedia.com>
2014-05-09 13:05:53 +0100

    Added response classes

---
+++ src/Responses/ResponseMessage.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace PhalconAPI\Responses;
+
+
+class ResponseMessage
+{
+  const TYPE_SUCCESS = 'success';
+  const TYPE_WARNING = 'warning';
+  const TYPE_INFO = 'info';
+  const TYPE_ALERT = 'alert';
+  const TYPE_SECONDARY = 'secondary';
+
+  public $text;
+  public $type;
+
+  public function __construct($text, $type = self::TYPE_INFO)
+  {
+    $this->text = $text;
+    $this->type = $type;
+  }
+}
---
+++ src/Responses/ResponseMeta.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace PhalconAPI\Responses;
+
+
+class ResponseMeta
+{
+  /** @var int */
+  public $status;
+
+  /** @var int */
+  public $count = 0;
+}
