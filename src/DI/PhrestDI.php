d6548121447a244babcb7cb604c62a67814e6b7f
Jay Francis <jay@whistlingmedia.com>
2014-06-09 18:42:43 +0100

    Initial oAuth2 implementation

--- src/DI/PhrestDI.php
+++ src/DI/PhrestDI.php
@@ -2,60 +2,75 @@
 
 namespace PhrestAPI\DI;
 
+use League\OAuth2\Server\Authorization;
+use League\OAuth2\Server\Grant\ClientCredentials;
+use Phalcon\Db\Adapter\Pdo\Mysql;
 use Phalcon\DI\FactoryDefault as PhalconDI;
+use Phalcon\DI;
 use Phalcon\Events\Manager as EventsManager;
 use Phalcon\Http\Request;
 use Phalcon\Mvc\Dispatcher as MVCDispatcher;
 use Phalcon\Mvc\Dispatcher;
+use Phalcon\Mvc\Router;
 use Phalcon\Registry;
+use PhrestAPI\Oauth2\Server\Storage\Pdo\Mysql\Client;
+use PhrestAPI\Oauth2\Server\Storage\Pdo\Mysql\Scope;
+use PhrestAPI\Oauth2\Server\Storage\Pdo\Mysql\Session;
 use PhrestAPI\Request\PhrestRequest;
 use PhrestAPI\Responses\CSVResponse;
 use PhrestAPI\Responses\JSONResponse;
 use PhrestAPI\Responses\Response;
-use WZSDK\SDK;
 
 /**
  * PhrestDI
  */
-class PhrestDI extends PhalconDI
+class PhrestDI extends DI
 {
   /**
    * Construct all of the dependencies for the API
    */
   public function __construct()
   {
-    parent::__construct();
-
     // Prepare the request object
-    $this->set(
-      'request',
-      function()
-      {
-        return new PhrestRequest();
-      }
-    );
+    $this['request'] = function ()
+    {
+      return new PhrestRequest();
+    };
 
-    // Prepare the Response object
-    /*$this->set(
-      'response',
-      function ()
-      {
-        // Prepare relevant response object
-        switch($this->get('request')->get('type'))
-        {
-          case Response::TYPE_RAW:
-            return new Response();
-            break;
-          case Response::TYPE_CSV:
-            return new CSVResponse();
-            break;
-          case Response::TYPE_JSON:
-          default:
-            return new JSONResponse();
-            break;
-        }
-      },
-      true
-    );*/
+    // Set up oAuth2 service
+    $this['oauth2'] = function ()
+    {
+      $config = $this->get('config');
+      $oauth2DB = new Mysql($config->oauth2DB->toArray());
+
+      // Create oAuth2 server
+      $server = new Authorization(
+        new Client($oauth2DB),
+        new Session($oauth2DB),
+        new Scope($oauth2DB)
+      );
+
+      # Not required as it called directly from original code
+      # $request = new \League\OAuth2\Server\Util\Request();
+
+      # add these 2 lines code if you want to use my own Request otherwise comment it
+      // todo required?
+      //$request = new \PhrestAPI\Oauth2\Server\Request();
+      //$server->setRequest($request);
+
+      $server->setAccessTokenTTL(86400);
+      $server->addGrantType(new ClientCredentials());
+
+      return $server;
+    };
+
+    // Default router
+    $this['router'] = function ()
+    {
+      return new Router;
+    };
+
+    // Set this as the Default DI
+    $this->setDefault($this);
   }
 }
